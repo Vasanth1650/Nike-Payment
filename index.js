b0a54058a49b6c8397ab2f15054ab1672099c2a7
Vasanth1650 <vasanth16756@gmail.com>
2022-06-29 21:09:07 +0530

    please

--- index.js
+++ index.js
@@ -40,4 +40,6 @@ app.post('/razorpay' ,async(req,res)=>{
   
 })
 
-.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
